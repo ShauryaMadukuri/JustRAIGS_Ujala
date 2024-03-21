a6d783fe18b765cfc66c44d5f6cf4ab6e227a27e
ShauryaMadukuri <shauryamadukuri@gmail.com>
2024-03-22 03:54:24 +0530

    code mistake

--- inference.py
+++ inference.py
@@ -59,6 +59,29 @@ def get_result_task2(processor,model,checkpoint_path,image_path):
 
 def run():
     _show_torch_cuda_info()
+    model_directory = "swinv2_tiny_model"
+    # Load the configuration file
+    config = AutoConfig.from_pretrained(model_directory)
+
+    # Load the image processor if needed
+    processor = AutoImageProcessor.from_pretrained(model_directory)
+
+    # Load the model checkpoint
+    model = AutoModelForImageClassification.from_pretrained(model_directory, config=config)
+
+    checkpoint_path = 'checkpoints/swin_finetunetask11.pth'
+
+    model_directory_2 = "swinv2_tiny_model"
+    # Load the configuration file
+    config_2 = AutoConfig.from_pretrained(model_directory_2)
+
+    # Load the image processor if needed
+    processor_2 = AutoImageProcessor.from_pretrained(model_directory_2)
+
+    # Load the model checkpoint
+    model_2 = AutoModelForImageClassification.from_pretrained(model_directory_2, config=config_2)
+
+    checkpoint_path_2 = 'checkpoints/swin_finetunetask219.pth'
 
     for jpg_image_file_name, save_prediction in inference_tasks():
         # Do inference, possibly something better performant
@@ -69,17 +92,7 @@ def run():
         # # For example: use Pillow to read the jpg file and convert it to a NumPY array:
         # image = Image.open(jpg_image_file_name)
         # numpy_array = numpy.array(image)
-        model_directory = "swinv2_tiny_model"
-        # Load the configuration file
-        config = AutoConfig.from_pretrained(model_directory)
-
-        # Load the image processor if needed
-        processor = AutoImageProcessor.from_pretrained(model_directory)
-
-        # Load the model checkpoint
-        model = AutoModelForImageClassification.from_pretrained(model_directory, config=config)
-
-        checkpoint_path = 'checkpoints/swin_finetunetask11.pth'
+        
         image_path=jpg_image_file_name
 
         result_task1 = get_result_task1(processor,model,checkpoint_path,image_path)
@@ -90,24 +103,18 @@ def run():
         is_referable_glaucoma = predicted_label
         if is_referable_glaucoma:
             # Define the local directory path where your model is saved
-            model_directory_2 = "swinv2_tiny_model"
-            # Load the configuration file
-            config_2 = AutoConfig.from_pretrained(model_directory_2)
-
-            # Load the image processor if needed
-            processor_2 = AutoImageProcessor.from_pretrained(model_directory_2)
-
-            # Load the model checkpoint
-            model_2 = AutoModelForImageClassification.from_pretrained(model_directory_2, config=config_2)
-
-            checkpoint_path_2 = 'checkpoints/swin_finetunetask219.pth'
+            
             result_task2 = get_result_task2(processor_2,model_2,checkpoint_path_2,image_path)
             features = {
                 k: v
                 for k, v in zip(DEFAULT_GLAUCOMATOUS_FEATURES.keys(), result_task2)
             }
         else:
-            features = None
+            # make all null
+            features = {
+                k: None
+                for k in DEFAULT_GLAUCOMATOUS_FEATURES.keys()
+            }
         ...
 
         # Finally, save the answer
