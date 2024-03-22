32bdf40a119ce7d7a3e8df27532b309a6f143a40
ShauryaMadukuri <shauryamadukuri@gmail.com>
2024-03-23 01:06:41 +0530

    path modified

--- inference.py
+++ inference.py
@@ -14,6 +14,10 @@ import os
 # Set the environment variable TRANSFORMERS_OFFLINE to 1
 os.environ['TRANSFORMERS_OFFLINE'] = '1'
 print(f"TRANSFORMERS_OFFLINE: {os.environ.get('TRANSFORMERS_OFFLINE')}")
+import os
+
+
+
 # # Set the environment variable HF_HOME to the cache directory
 # os.environ['HF_HOME'] = '/cache/huggingface'
 # print(f"HF_HOME: {os.environ.get('HF_HOME')}")
@@ -46,7 +50,8 @@ def get_result_task2(processor,model,checkpoint_path,image_path):
     model.load_state_dict(torch.load(checkpoint_path))
 
     # Assuming you have a validation DataLoader named val_loader
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = 'cpu'
     model.to(device)
 
     # Image processing
@@ -69,29 +74,50 @@ def get_result_task2(processor,model,checkpoint_path,image_path):
 
 def run():
     _show_torch_cuda_info()
-    model_directory = "swinv2_tiny_model"
-    # Load the configuration file
-    config = AutoConfig.from_pretrained(os.path.join(model_directory, "config.json"), local_files_only=True)
+    # Get the current working directory
+    current_directory = os.getcwd()
+
+# Print the current working directory
+    print("Current working directory:", current_directory)
+    # Folder to join
+    folder_to_join = "swinv2_tiny_model"
+
+    # Join the folder to the current working directory
+    model_directory = os.path.join(current_directory, folder_to_join)
+
+    # Print the model_directory path
+    print("model_directory =", model_directory)
 
     # Load the image processor if needed
-    processor = AutoImageProcessor.from_pretrained(model_directory, local_files_only=True)
+    processor = AutoImageProcessor.from_pretrained(model_directory)
 
     # Load the model checkpoint
-    model = AutoModelForImageClassification.from_pretrained(model_directory, config=config, local_files_only=True)
+    model = AutoModelForImageClassification.from_pretrained(model_directory)
 
-    checkpoint_path = 'checkpoints/swin_finetunetask11.pth'
+    # Path of the checkpoint relative to the current directory
+    checkpoint_path_relative = 'checkpoints/swin_finetunetask11.pth'
+
+    # Join the checkpoint path with the current working directory
+    checkpoint_path = os.path.join(current_directory, checkpoint_path_relative)
+
+    # Print the checkpoint path
+    print("checkpoint_path =", checkpoint_path)
+
 
     model_directory_2 = "swinv2_tiny_model"
-    # Load the configuration file
-    config_2 = AutoConfig.from_pretrained(os.path.join(model_directory_2, "config.json"), local_files_only=True)
+
+    model_directory_2 = os.path.join(current_directory, model_directory_2)
 
     # Load the image processor if needed
-    processor_2 = AutoImageProcessor.from_pretrained(model_directory_2, local_files_only=True)
+    processor_2 = AutoImageProcessor.from_pretrained(model_directory_2)
 
     # Load the model checkpoint
-    model_2 = AutoModelForImageClassification.from_pretrained(model_directory_2, config=config_2, local_files_only=True)
+    model_2 = AutoModelForImageClassification.from_pretrained(model_directory_2)
 
     checkpoint_path_2 = 'checkpoints/swin_finetunetask219.pth'
+
+    checkpoint_path_2 = os.path.join(current_directory, checkpoint_path_2)
+    
 
     for jpg_image_file_name, save_prediction in inference_tasks():
         # Do inference, possibly something better performant
